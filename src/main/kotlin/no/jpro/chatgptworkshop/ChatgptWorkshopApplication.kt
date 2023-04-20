5e63ee7c1d0d339931c35663f8535e852cc7a786
Fredrik Haugseth <fredrik@jpro.no>
2023-04-20 14:13:13 +0200

    Set up a starting point for a Kotlin application

---
+++ src/main/kotlin/no/jpro/chatgptworkshop/ChatgptWorkshopApplication.kt
@@ -0,0 +1,11 @@
+package no.jpro.chatgptworkshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatgptWorkshopApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChatgptWorkshopApplication>(*args)
+}
---
+++ src/test/kotlin/no/jpro/chatgptworkshop/ChatgptWorkshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.jpro.chatgptworkshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatgptWorkshopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
